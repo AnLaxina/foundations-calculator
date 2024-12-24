fe351826276524b077ddc468d81b880a6b337247
AnLaxina <anilovlaxina@gmail.com>
2024-12-24 13:35:43 -0800

    Create working operations for add, subtract, multiply, & divide

---
+++ JS/calculator.js
@@ -0,0 +1,18 @@
+// This JavaScript file is only concerned with the operations of how the calculator should function
+
+// To start, here are the operator functions I'll use later
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+} 
