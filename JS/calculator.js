37e13ee876ec4d29278e759ab852e745dc1bd030
AnLaxina <anilovlaxina@gmail.com>
2024-12-27 09:32:03 -0800

    Create working operate function
    Right now all it does is perform the operations based on the button pressed

--- JS/calculator.js
+++ JS/calculator.js
@@ -17,6 +17,27 @@ function divide(a, b) {
     return a / b;
 }
 
+// Takes in two numbers and uses the above operator functions based on the clicked button
+function operate(a, b, operator) {
+    let answer = 0;
+    switch (operator) {
+        case "+":
+            answer = add(a, b);
+            break;
+        case "-":
+            answer = subtract(a, b);
+            break;
+        case "*":
+            answer = multiply(a, b);
+            break;
+        case "/":
+            answer = divide(a, b);
+            break;
+
+    }
+    return answer;
+}
+
 // Handle button press for each button and change the display accordingly
 const numbers = document.querySelector(".numbers");
 const displayP = document.querySelector(".display p");
@@ -36,6 +57,9 @@ numbers.addEventListener("click", (e) => {
     switch (e.target.textContent) {
         case "+":
             console.log("Hello + was pressed");
+            break;
+        case "=":
+            console.log(operate(1, 2, "+"));
             break;
     }
 
