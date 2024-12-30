95a47070b3ad460dcf16815003b0859f38adf612
AnLaxina <anilovlaxina@gmail.com>
2024-12-29 22:06:12 -0800

    Add very basic functionality for highlighting the operator buttons
    Commented the logic behind populating the display as I couldn't get it to work with the highlights

--- JS/calculator.js
+++ JS/calculator.js
@@ -38,6 +38,17 @@ function operate(a, b, operator) {
     return answer;
 }
 
+function changeButtonHighlight(e) {
+    if (operators.includes(e.target.textContent) && !alreadyHighlighted) {
+        e.target.style.backgroundColor = "#B17E2B";
+        alreadyHighlighted = true;
+    }
+    else {
+        e.target.style.backgroundColor = "#de9e36ff";
+        alreadyHighlighted = false;
+    }
+}
+
 // Handle button press for each button and change the display accordingly
 const numbers = document.querySelector(".numbers");
 const displayP = document.querySelector(".display p");
@@ -47,39 +58,43 @@ const operators = ["*", "+", "-", "/"];
 const otherOperators = ["AC", "="];
 
 let firstTime = true;
+let alreadyHighlighted = false;
 let firstOperand = undefined;
 let secondOperand = 0;
 
-numbers.addEventListener("click", (e) => {
+numbers.addEventListener("click", changeButtonHighlight);
 
-    switch (e.target.textContent) {
-        case "+":
-            console.log("Hello + was pressed");
-            break;
-        case "=":
-            console.log(operate(1, 2, "+"));
-            break;
-    }
+// numbers.addEventListener("click", (e) => {
 
-    // Prevent any of the operators in the calculator to be shown on the display
-    // If any operator is pressed, change the colour to be clicked and create a variable for a number
-    if (operators.includes(e.target.textContent)) {
-        // If the first number has never been used before, set it to whatever the user inputs first
-        if (typeof firstOperand === "undefined") {
-            firstOperand = parseInt(displayP.textContent);
-            console.log(`The value of firstOperand is ${firstOperand}, and it's type is: ${typeof firstOperand}`);
-        }
-    }
-    else if (otherOperators.includes(e.target.textContent)) {
-        console.log(`You pressed ${e.target.textContent}`);
-    }
-    else {
-        // Before you add to the display, delete the placeholder
-        if (firstTime) {
-            displayP.textContent = "";
-            firstTime = false;
-        }
-        displayP.textContent += e.target.textContent;
-    }
+//     switch (e.target.textContent) {
+//         case "+":
+//             break;
+//         case "=":
+//             break;
+//     }
 
-})
+//     changeButtonHighlight(e);
+
+//     // Prevent any of the operators in the calculator to be shown on the display
+//     // If any operator is pressed, change the colour to be clicked and create a variable for a number
+//     if (operators.includes(e.target.textContent)) {
+//         // If the first number has never been used before, set it to whatever the user inputs first
+//         if (typeof firstOperand === "undefined") {
+//             firstOperand = parseInt(displayP.textContent);
+//             console.log(`The value of firstOperand is ${firstOperand}, and it's type is: ${typeof firstOperand}`);
+//         }
+
+//     }
+//     else if (otherOperators.includes(e.target.textContent)) {
+//         console.log(`You pressed ${e.target.textContent}`);
+//     }
+//     else {
+//         // Before you add to the display, delete the placeholder
+//         if (firstTime) {
+//             displayP.textContent = "";
+//             firstTime = false;
+//         }
+//         displayP.textContent += e.target.textContent;
+//     }
+
+// })
